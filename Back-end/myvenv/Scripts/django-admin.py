b4fefddd669a741411ea9426ccc031acb1b8cc3a
OmaraliA <aru.omarali@gmail.com>
2018-04-17 20:13:12 +0600

    updates

---
+++ Back-end/myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\андрей\desktop\web\weblastversion\back-end\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
